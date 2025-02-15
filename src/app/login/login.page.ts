e322e8ac15ebed2f26f68f80c23d6c2701a67977
AnaVel2002 <Anavel.2002@proton.me>
2025-02-15 03:41:20 -0600

    [Add] -Loading Component To Start

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { InfoModalComponent } from '../info-modal/info-modal.component';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -17,8 +19,11 @@ export class LoginPage {
   password: string = '';
   isValid: boolean = false;
 
-  constructor(private modalController: ModalController) {}
+  constructor(private modalController: ModalController, private router: Router, private loadingController: LoadingController) {}
 
+  usuarios = [
+    {username: 'ana', password: '1234'}
+  ];
   validarCampos() {
     this.username = this.username.replace(/\s+/g, '').toLowerCase();
     this.password = this.password.replace(/\s+/g, '');
@@ -35,5 +40,29 @@ export class LoginPage {
       },
     });
     return await modal.present();
+  }
+// Author: Velázque Gutiérrez Ana Karen
+  Iniciar() {
+    const usuarioEncontrado = this.usuarios.find(user => 
+      user.username === this.username && user.password === this.password
+    );
+  
+    if (usuarioEncontrado) {
+      this.presentLoading('Accediendo...', () => {
+        this.router.navigate(['/home']); // Redirige a Home
+      });
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
+  }
+//Author: Velázque Gutiérrez Ana Karen
+  async presentLoading(mensaje: string, callback: Function) {
+    const loading = await this.loadingController.create({
+      message: mensaje,
+      duration: 3000 // 3 segundos
+    });
+    await loading.present();
+    await loading.onDidDismiss();
+    callback();
   }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -15,7 +17,7 @@ import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angula
 export class RegisterPage implements OnInit {
   registerForm: FormGroup;
 registros: any[]=[];
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router, private loadingController: LoadingController) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required]],
@@ -55,12 +57,44 @@ registros: any[]=[];
   get email() { return this.registerForm.get('email'); }
   get user() { return this.registerForm.get('user'); }
 
-
-  register() {
+    //Author: Velázque Gutiérrez Ana Karen
+  async register() {
     if (this.registerForm.valid) {
-      this.registros.push(this.registerForm.value);//AQUI ES DONDE SE AGREGAN AL ARREGLO registros
+      const loading = await this.presentLoading('Espere...');
+
+      setTimeout(async () => {
+        this.registros.push(this.registerForm.value);//AQUI ES DONDE SE AGREGAN AL ARREGLO registros
       console.log('Datos del usuario:', this.registerForm.value);
       alert('Registro exitoso');
+
+        this.registerForm.reset();
+        await loading.dismiss();
+
+        this.router.navigate(['/login']);
+      }, 3000); // 3 segundos
     }
+  }
+  //Author: Velázque Gutiérrez Ana Karen
+  async presentLoading(message: string) {
+    const loading = await this.loadingController.create({
+      message: message,
+      spinner: 'crescent',
+      backdropDismiss: false,
+      duration: 0
+    });
+
+    const loadingElement = await loading.present();
+
+    // Logo de la UTEQ
+    const content = document.querySelector('.loading-content');
+    if (content) {
+      content.innerHTML = `
+        <img src="/assets/LOGO.png" class="loading-image" alt="UTEQ">
+        <div class="loading-text" style="display: flex; justify-content: center; text-align: center; 15px; color: #313;">
+            ${message}
+        </div>
+      `;
+    }
+    return loading;
   }
 }
